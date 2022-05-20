332fa360a7a9d49b017b043ad5a0df9c2063a84f
Company Aigniter <companyaigniter@Companys-MacBook-Air-4.local>
2022-05-20 18:46:17 +0800

    add config module and .developement.env

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,27 @@ import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 import { AuthModule } from "./auth/auth.module";
 import { UserModule } from "./user/user.module";
-import { TaskModule } from './task/task.module';
+import { TaskModule } from "./task/task.module";
+import { ConfigModule } from "@nestjs/config";
+import ormconfig from "../ormconfig";
 @Module({
-  imports: [TypeOrmModule.forRoot(), UserModule, AuthModule, TaskModule],
+  imports: [
+    ConfigModule.forRoot({ envFilePath: ".development.env" }),
+    TypeOrmModule.forRoot({
+      type: "mysql",
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT, 10),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: "my_app",
+      entities: ["dist/**/*entity.js"],
+      migrations: ["dist/migrations/*.js"],
+      synchronize: true,
+    }),
+    UserModule,
+    AuthModule,
+    TaskModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
